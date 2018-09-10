125b7442b2833fe2a4c115428a4ba88324e3fec7
William Holcombe <william.holcombe@uconn.edu>
2018-09-10 17:44:35 -0400

    master

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -28,6 +28,7 @@ export class Dashboard extends React.Component {
 const mapStateToProps = state => {
     const {currentUser} = state.auth;
     return {
+        
         username: state.auth.currentUser.username,
         name: `${currentUser.firstname} ${currentUser.lastName}`,
         protectedData: state.protectedData.data
