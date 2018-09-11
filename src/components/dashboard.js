67cbbe0dbf6a4e14458c3ef750fe7c6f23436033
Brandon Graham <bgraham452@gmail.com>
2018-09-11 09:57:22 -0400

    set up static elements of dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -11,6 +11,12 @@ export class Dashboard extends React.Component {
     render() {
         return (
             <div className="dashboard">
+                <main className="main-img-section">
+
+            <img src={require('../images/dothraki-main.jpg')} alt="Dothraki Horde"  className="main-img"/>
+
+            </main>
+               
                 <div className="dashboard-username">
                     Username: {this.props.username}
                 </div>
