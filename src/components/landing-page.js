ad406252130eb3df7fc1a74f950330592629ac6e
rpinto <rachel.lea.pinto@gmail.com>
2018-09-13 22:28:59 -0700

    landing page very rough draft

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -23,24 +23,30 @@ export function LandingPage(props) {
             <section className="description-row-1">
                 <div className="column">
                     <h3 className="title">You want to learn to speak Dothraki?</h3>
-                        <p className="text">
-                            Once you register, you'll be provided your own dashboard full of popular vocabulary and useful phrases. LearnDothraki gives you an edge by noticing which words you excel in, and which may need some extra revision.
-                        </p>
                     
                 </div>
 
                 <div className="column">
-                    <h3 className="title2">Spaced Repetition</h3>
-                    <div className="text">
-                        <p>
+                <p className="text">
+                            Once you register, you'll be provided your own dashboard full of popular vocabulary and useful phrases. LearnDothraki gives you an edge by noticing which words you excel in, and which may need some extra revision.
+                        </p>
+                </div>
+            </section>
+            <section className="description-row-1">
+                <div className="column">
+                <p>
                             We use spaced repetition to optimize your ability to retain information in the long term. You don't have to worry about organizing flash cards- this program will guide you to what you need to practice, till you get it right every time.
                         </p>
+                </div>
+                <div className="column">
+                    <h3 className="title2">Spaced Repetition</h3>
+                    <div className="text">
                     </div>
                 </div>
             </section>
             <section className="description-row-2">
                 <div className="how-to">
-                    <h3 className="title">How to use: </h3>
+                    <h3 className="title3">How to use: </h3>
                     <div className="text">
                         <p>
                             It's simple, we'll present a word in Dothraki, you type in what it means in English. We'll help you learn faster, so you have more time for dragon taming.
@@ -48,7 +54,9 @@ export function LandingPage(props) {
                     </div>
                 </div>
             </section>
+            <div className="footer"></div>
         </div>
+       
     );
 }
 
