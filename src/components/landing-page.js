94ca1dfa3d811a57f702a93fe6d33e6362519468
rpinto <rachel.lea.pinto@gmail.com>
2018-09-13 21:16:54 -0700

    non responsive landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import './styles/landing-page.css'
 export function LandingPage(props) {
@@ -10,58 +10,44 @@ export function LandingPage(props) {
     }
 
     return (
+       
         <div className="home">
 
-       <main className="main-img-section">
-            <h2 className="headline-text">Learn the language of the Horselords</h2>
-
-            <img src={require('../images/dothraki-main.jpg')} alt="Dothraki Horde"  className="main-img"/>
-
-            <button className="begin"><Link to="/register">Begin</Link></button>
+            <main className="main-img-section">
+                <img src={require('../images/dothrakhorses.jpg')} alt="Dothraki horses statue" className="main-img darken" />
+                <li><img src={require('../images/LearningDothraki.png')} alt="learn dothraki logo" className="logo"/></li>
+                <h2 className="headline-text">Face the other bloodlords with confidence</h2>
+                <a className="begin"><Link to="/register">BEGIN</Link></a>
             </main>
 
-<section className="descriptions">
-<div className="space-rep">
-<h3 className="title">Spaced Repetition</h3>
-<div className="text">
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-</p>
+            <section className="description-row-1">
+                <div className="column">
+                    <h3 className="title">You want to learn to speak Dothraki?</h3>
+                        <p className="text">
+                            Once you register, you'll be provided your own dashboard full of popular vocabulary and useful phrases. LearnDothraki gives you an edge by noticing which words you excel in, and which may need some extra revision.
+                        </p>
+                    
+                </div>
 
-<p>
-
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</p>
-</div>
-</div>
-
-<div className="track-progress">
-<h3 className="title">Track Progress</h3>
-<div className="text">
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-</p>
-<p>
-
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</p>
-</div>
-</div>
-<div className="fully-custom">
-<h3 className="title">Fully Customizable</h3>
-<div className="text">
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-</p>
-<p>
-
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</p>
-</div>
-</div>
-</section>
-       
-            {/* <LoginForm /> */}
+                <div className="column">
+                    <h3 className="title2">Spaced Repetition</h3>
+                    <div className="text">
+                        <p>
+                            We use spaced repetition to optimize your ability to retain information in the long term. You don't have to worry about organizing flash cards- this program will guide you to what you need to practice, till you get it right every time.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="description-row-2">
+                <div className="how-to">
+                    <h3 className="title">How to use: </h3>
+                    <div className="text">
+                        <p>
+                            It's simple, we'll present a word in Dothraki, you type in what it means in English. We'll help you learn faster, so you have more time for dragon taming.
+                        </p>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
