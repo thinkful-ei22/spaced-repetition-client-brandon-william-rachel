1ab4115826fef0c94cb05ab984c80c9fc945b840
rpinto <rachel.lea.pinto@gmail.com>
2018-09-14 12:14:13 -0700

    merged r and b work

--- src/components/app.js
+++ src/components/app.js
@@ -49,7 +49,7 @@ export class App extends React.Component {
     render() {
         return (
             <div className="app" lang="en">
-                <DashHeaderBar />
+                <Route exact path="/dashboard" component={DashHeaderBar}/>
                 <Route exact path="/" component={LandingPage} />
                 <Route exact path="/dashboard" component={Dashboard} />
                 <Route exact path="/register" component={RegistrationPage} />
---
+++ src/components/header-bar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
+import {Link} from 'react-router-dom';
+import './styles/header-bar.css';
+
+export class HeaderBar extends React.Component {
+    logOut() {
+        this.props.dispatch(clearAuth());
+        clearAuthToken();
+    }
+
+    render() {
+        console.log(this.props,"=======")
+        // Only render the log out button if we are logged in
+        let logOutButton, signUpButton, logInButton, aboutToggle;
+        if (this.props.loggedIn) {
+            logOutButton = (
+                <button className="logout" onClick={() => this.logOut()}>Log out</button>
+            );
+        }
+        else {
+            signUpButton =(
+            <button className="signup">Sign Up</button>
+            );
+            logInButton =(
+                <button className="login">Log In</button>
+                );
+            aboutToggle =(
+                <button className="about" onClick={() => this.props.switchOverlay(true)}>About</button>
+            );
+        }
+        return (
+            <section className="header-bar">
+                <ul>
+                    <Link to="/register"><li>{signUpButton}</li></Link>
+                    <Link to="/login"><li>{logInButton}</li></Link>
+                    {aboutToggle}
+                    {logOutButton}
+                </ul>
+            </section>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(HeaderBar);
